8e6efb57d59849f0af2190f1d60390e437994e8a
Nel Konigg <alenkobalenko007@gmail.com>
2026-02-14 16:55:54 +0100

    Fixed recipes to work with JEI.

--- src/main/java/nel/bettershield/registry/BetterShieldItems.java
+++ src/main/java/nel/bettershield/registry/BetterShieldItems.java
@@ -3,7 +3,7 @@ package nel.bettershield.registry;
 import nel.bettershield.Bettershield;
 import nel.bettershield.item.ModShieldItem;
 import nel.bettershield.recipe.BetterShieldDecorationRecipe;
-import nel.bettershield.recipe.TemplatelessSmithingRecipe;
+// REMOVED: import nel.bettershield.recipe.TemplatelessSmithingRecipe;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
@@ -17,24 +17,24 @@ import net.minecraft.util.Identifier;
 
 public class BetterShieldItems {
 
-    // Diamond: +30% Durability, +15% Damage, -10% Cooldown, Enchantability 10 (Standard Diamond)
+    // Diamond: Enchantability 10
     public static final Item DIAMOND_SHIELD = new ModShieldItem(
             new FabricItemSettings().maxCount(1),
             437,
             0.15f,
             0.10f,
             false,
-            10 // NEW: Enchantability
+            10
     );
 
-    // Netherite: +60% Durability, +25% Damage, -20% Cooldown, Fireproof, Enchantability 15 (Standard Netherite)
+    // Netherite: Enchantability 15
     public static final Item NETHERITE_SHIELD = new ModShieldItem(
             new FabricItemSettings().maxCount(1).fireproof(),
             538,
             0.25f,
             0.20f,
             true,
-            15 // NEW: Enchantability
+            15
     );
 
     public static final RecipeSerializer<BetterShieldDecorationRecipe> SHIELD_DECORATION_SERIALIZER = new SpecialRecipeSerializer<>(BetterShieldDecorationRecipe::new);
@@ -43,7 +43,8 @@ public class BetterShieldItems {
         Registry.register(Registries.ITEM, new Identifier(Bettershield.MOD_ID, "diamond_shield"), DIAMOND_SHIELD);
         Registry.register(Registries.ITEM, new Identifier(Bettershield.MOD_ID, "netherite_shield"), NETHERITE_SHIELD);
 
-        Registry.register(Registries.RECIPE_SERIALIZER, new Identifier(Bettershield.MOD_ID, "smithing_no_template"), TemplatelessSmithingRecipe.Serializer.INSTANCE);
+        // REMOVED: The custom "smithing_no_template" serializer registration
+
         Registry.register(Registries.RECIPE_SERIALIZER, new Identifier(Bettershield.MOD_ID, "shield_decoration"), SHIELD_DECORATION_SERIALIZER);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
