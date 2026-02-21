98eb06dcd5154cbe78762b8a2733893100a9aff0
Nel Konigg <alenkobalenko007@gmail.com>
2026-02-21 22:07:48 +0100

    halfway port

--- src/main/java/nel/bettershield/registry/BetterShieldEnchantments.java
+++ src/main/java/nel/bettershield/registry/BetterShieldEnchantments.java
@@ -2,51 +2,23 @@ package nel.bettershield.registry;
 
 import nel.bettershield.Bettershield;
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class BetterShieldEnchantments {
 
-    // --- 1.20.5 FIX: Drastically raised the "Max Power" ceiling so Netherite doesn't overflow! ---
-    public static final Enchantment SHIELD_DENSITY = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            10, 3, Enchantment.leveledCost(1, 11), Enchantment.leveledCost(51, 11), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
+    // --- 1.21 FIX: We now just create "Keys" that point to your future JSON files! ---
+    public static final RegistryKey<Enchantment> SHIELD_DENSITY = of("shield_density");
+    public static final RegistryKey<Enchantment> PARRYFUL = of("parryful");
+    public static final RegistryKey<Enchantment> DEFLECTOR = of("deflector");
+    public static final RegistryKey<Enchantment> PARRY_DOCTRINE = of("parry_doctrine");
+    public static final RegistryKey<Enchantment> SLAM_FOAM = of("slam_foam");
+    public static final RegistryKey<Enchantment> MASTERINE = of("masterine");
+    public static final RegistryKey<Enchantment> ACTIVE_ARMOR = of("active_armor");
 
-    public static final Enchantment PARRYFUL = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            5, 3, Enchantment.leveledCost(5, 9), Enchantment.leveledCost(55, 9), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-    public static final Enchantment DEFLECTOR = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            2, 2, Enchantment.leveledCost(10, 15), Enchantment.leveledCost(60, 15), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-    public static final Enchantment PARRY_DOCTRINE = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            2, 2, Enchantment.leveledCost(12, 18), Enchantment.leveledCost(62, 18), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-    public static final Enchantment SLAM_FOAM = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            5, 2, Enchantment.leveledCost(8, 14), Enchantment.leveledCost(58, 14), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-    public static final Enchantment MASTERINE = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            2, 2, Enchantment.leveledCost(15, 15), Enchantment.leveledCost(65, 15), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-    public static final Enchantment ACTIVE_ARMOR = new Enchantment(Enchantment.properties(
-            ItemTags.DURABILITY_ENCHANTABLE,
-            1, 3, Enchantment.leveledCost(20, 12), Enchantment.leveledCost(70, 12), 1, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND));
-
-
-    public static void register() {
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "shield_density"), SHIELD_DENSITY);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "parryful"), PARRYFUL);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "deflector"), DEFLECTOR);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "parry_doctrine"), PARRY_DOCTRINE);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "slam_foam"), SLAM_FOAM);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "masterine"), MASTERINE);
-        Registry.register(Registries.ENCHANTMENT, new Identifier(Bettershield.MOD_ID, "active_armor"), ACTIVE_ARMOR);
+    private static RegistryKey<Enchantment> of(String name) {
+        // In 1.21, Mojang changed "new Identifier()" to "Identifier.of()"
+        return RegistryKey.of(RegistryKeys.ENCHANTMENT, Identifier.of(Bettershield.MOD_ID, name));
     }
 }
